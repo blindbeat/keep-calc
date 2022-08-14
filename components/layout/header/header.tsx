f26dbb42667f1ce822cced432e2e90311c0b0b31
bb <oleh.kvsk@gmail.com>
2022-08-15 01:28:40 +0300

    header, fonts, icons

--- components/layout/header/header.tsx
+++ components/layout/header/header.tsx
@@ -1,5 +1,12 @@
 import React from "react"
+import classNames from "classnames"
+import styles from "./Header.module.css"
 
-const Header = () => <header>header</header>
+const Header = () => (
+  <header className={styles.header}>
+    <span className={styles.menu}>keep.treat</span>
+    <span className={classNames("material-icons", styles.menu)}>menu</span>
+  </header>
+)
 
 export default Header
---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+import { Html, Head, Main, NextScript } from "next/document"
+
+const Document = () => (
+  <Html>
+    <Head>
+      <link
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        rel="stylesheet"
+      />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
+        rel="stylesheet"
+      />
+    </Head>
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+)
+
+export default Document
